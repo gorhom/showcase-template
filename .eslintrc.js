4e7e46789b691e247fcdc6c7ba08ec58d5c493f5
gorhom <gorhom.dev@gmail.com>
2023-09-15 18:28:59 +0200

    chore: updated linter and package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'prettier'],
+  extends: ['@react-native', 'prettier'],
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 80,
-        arrowParens: 'avoid',
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: 'es5',
-        useTabs: false,
-      },
-    ],
+    'prettier/prettier': 'error',
   },
 };
